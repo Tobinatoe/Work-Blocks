9fb432f40fd142d82f5c14abc4f9863ed9ed3592
Tobias Leitner <nlemailtobi@gmail.com>
2025-08-15 12:31:14 +0200

    Initial Commit

---
+++ WorkBlocks_v2/WorkBlocks_v2App.swift
@@ -0,0 +1,17 @@
+//
+//  WorkBlocks_v2App.swift
+//  WorkBlocks_v2
+//
+//  Created by Tobias Leitner on 15.08.2025.
+//
+
+import SwiftUI
+
+@main
+struct WorkBlocks_v2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
